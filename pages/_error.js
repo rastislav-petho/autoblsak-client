8240984204883447318456ef265f6e24d5859c82
Rastislav Petho <rastislav.petho@gmail.com>
2020-10-04 14:42:48 +0200

    error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,13 +1,43 @@
+import React from 'react';
+import { Layout } from './../components';
+import Reveal from 'react-reveal/Reveal';
+
 function Error({ statusCode }) {
   return (
-    <>
-      <p>Ahoj</p>
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    </>
+    <Layout
+      pageTitle={`Stránka sa nenašla - Autoblšák.sk`}
+      pageDescription=""
+      pageKeywords=""
+    >
+      <Reveal>
+        <div className="row">
+          <div className="col-12 text-center">
+            <h2>OOOPS!!</h2>
+            <h4>
+              STRÁNKA, KTORÚ STE HĽADALI SA NENAŠLA PREJDITE PROSÍM NA NIEKTORÚ
+              ZO STRÁNOK
+            </h4>
+            <div className="row">
+              <div className="col-12 col-lg-4">
+                <Link href="/">
+                  <button className="button">INZERCIA</button>
+                </Link>
+              </div>
+              <div className="col-12 col-lg-4">
+                <Link href="https://autoblsak.sk/magazin">
+                  <button className="button">MAGAZÍN</button>
+                </Link>
+              </div>
+              <div className="col-12 col-lg-4">
+                <Link href="/kontakt">
+                  <button className="button">KONTAKT</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Reveal>
+    </Layout>
   );
 }
 
