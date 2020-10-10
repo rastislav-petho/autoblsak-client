2cd293316465cb07cdf04cf1c1568369a1041213
Rastislav Petho <rastislav.petho@gmail.com>
2020-10-10 13:57:45 +0200

    poslat predajcovi spravu ako modal

--- components/Ad/AdSendEmail.jsx
+++ components/Ad/AdSendEmail.jsx
@@ -56,79 +56,82 @@ export const AdSendEmail = (props) => {
 
   return (
     <>
-      {!sendEmail ? (
-        <button className="full-button mb-2" onClick={() => setSendEmail(true)}>
-          Poslať predajcovi správu
-        </button>
-      ) : (
-        <div className="card text-center w-100 mb-2">
-          <div className="card-header">
-            <h5>Poslať predajcovi správu</h5>
-          </div>
-          <div className="card-body text-left">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-group">
-                <label htmlFor="name">Vaše meno</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  name="name"
-                  value={emailState.name}
-                  onChange={(event) => handleChange(event)}
-                  ref={register({ required: true })}
-                />
-                {errors.name && errors.name.type === 'required' && (
-                  <i className="text-danger">Vaše meno je povinný údaj.</i>
-                )}
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Vaša e-mailová adresa</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                  value={emailState.email}
-                  onChange={(event) => handleChange(event)}
-                  ref={register({
-                    required: true,
-                    pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-                  })}
-                />
-                {(errors.email && errors.email.type === 'required' && (
-                  <i className="text-danger">E-mail je povinný.</i>
-                )) ||
-                  (errors.email && errors.email.type === 'pattern' && (
-                    <i className="text-danger">E-mail je v nesprávnom tvare.</i>
-                  ))}
-              </div>
+      <button className="full-button mb-2" onClick={() => setSendEmail(true)}>
+        Poslať predajcovi správu
+      </button>
+      {sendEmail && (
+        <div className="modal-window">
+          <div className="card text-center w-100 mb-2">
+            <div className="card-header">
+              <h5>Poslať predajcovi správu</h5>
+            </div>
+            <div className="card-body text-left">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                  <label htmlFor="name">Vaše meno</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    value={emailState.name}
+                    onChange={(event) => handleChange(event)}
+                    ref={register({ required: true })}
+                  />
+                  {errors.name && errors.name.type === 'required' && (
+                    <i className="text-danger">Vaše meno je povinný údaj.</i>
+                  )}
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Vaša e-mailová adresa</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={emailState.email}
+                    onChange={(event) => handleChange(event)}
+                    ref={register({
+                      required: true,
+                      pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+                    })}
+                  />
+                  {(errors.email && errors.email.type === 'required' && (
+                    <i className="text-danger">E-mail je povinný.</i>
+                  )) ||
+                    (errors.email && errors.email.type === 'pattern' && (
+                      <i className="text-danger">
+                        E-mail je v nesprávnom tvare.
+                      </i>
+                    ))}
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="message">Vaša správa</label>
-                <textarea
-                  className="form-control"
-                  id="message"
-                  name="message"
-                  rows="5"
-                  value={emailState.message}
-                  onChange={(event) => handleChange(event)}
-                  ref={register({ required: true })}
-                ></textarea>
-                {errors.message && errors.message.type === 'required' && (
-                  <i className="text-danger">Vaše správa je povinný údaj.</i>
-                )}
-              </div>
-              <button type="submit" className="button w-100 mb-2">
-                Odoslať
-              </button>
-            </form>
-          </div>
-          <div
-            className="card-footer text-muted cursor-pointer"
-            onClick={() => setSendEmail(false)}
-          >
-            zavrieť
+                <div className="form-group">
+                  <label htmlFor="message">Vaša správa</label>
+                  <textarea
+                    className="form-control"
+                    id="message"
+                    name="message"
+                    rows="5"
+                    value={emailState.message}
+                    onChange={(event) => handleChange(event)}
+                    ref={register({ required: true })}
+                  ></textarea>
+                  {errors.message && errors.message.type === 'required' && (
+                    <i className="text-danger">Vaše správa je povinný údaj.</i>
+                  )}
+                </div>
+                <button type="submit" className="full-button mb-2">
+                  Odoslať
+                </button>
+              </form>
+            </div>
+            <div
+              className="card-footer text-muted cursor-pointer"
+              onClick={() => setSendEmail(false)}
+            >
+              zavrieť
+            </div>
           </div>
         </div>
       )}
