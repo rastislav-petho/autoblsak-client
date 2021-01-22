254c08817b21bdebafe508f71c608851c9f5bcbd
Rastislav Petho <rastislav.petho@gmail.com>
2021-01-22 10:28:00 +0100

    zmena fontu

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -54,6 +54,11 @@ export const Layout = ({
         <meta name="description" content={pageDescription} />
         <meta name="keywords" content={pageKeywords} />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
         <link
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
