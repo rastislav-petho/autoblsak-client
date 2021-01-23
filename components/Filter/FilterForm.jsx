91f2a20bf84383f952c01f6fc96f604b8b355c42
Rastislav Petho <rastislav.petho@gmail.com>
2021-01-23 10:33:22 +0100

    uprava zobrazenia fieldov filter

--- components/Filter/FilterForm.jsx
+++ components/Filter/FilterForm.jsx
@@ -46,7 +46,7 @@ export const FilterForm = ({
           ))}
         </select>
       </div>
-      {filter.category == 1 && (
+      {['1'].includes(filter.category) && (
         <>
           <div className="form-group mb-2">
             <select
@@ -105,174 +105,200 @@ export const FilterForm = ({
           </div>
         </>
       )}
-      <div className="form-group mb-2">
-        <select onChange={handleChange} className="form-control" name="fuel">
-          <option value={filter.fuel ? filter.fuel : null}>
-            {filter.fuel ? decodeFuel(filter.fuel) : 'Palivo'}
-          </option>
-          <option value="">-- Palivo --</option>
-          {FUEL.map((item) => (
-            <option key={item.value} value={item.value}>
-              {item.label}
+      {['1', '2', '3', '4', '5', '6'].includes(filter.category) && (
+        <div className="form-group mb-2">
+          <select onChange={handleChange} className="form-control" name="fuel">
+            <option value={filter.fuel ? filter.fuel : null}>
+              {filter.fuel ? decodeFuel(filter.fuel) : 'Palivo'}
             </option>
-          ))}
-        </select>
-      </div>
-      <div className="form-group mb-2">
-        <select
-          onChange={handleChange}
-          className="form-control"
-          name="transmision"
-        >
-          <option value={filter.transmision ? filter.transmision : null}>
-            {filter.transmision
-              ? decodeTransmision(filter.transmision)
-              : 'Prevodovka'}
-          </option>
-          <option value="">-- Prevodovka --</option>
-          {TRANSMISION.map((trans) => (
-            <option key={trans.value} value={trans.value}>
-              {trans.label}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="form-group mb-2">
-        <select onChange={handleChange} className="form-control" name="color">
-          <option value={filter.color ? filter.color : null}>
-            {filter.color ? decodeColor(filter.color) : 'Farba'}
-          </option>
-          <option value="">-- Farba --</option>
-          {COLORS.map((color, index) => (
-            <option key={index} value={color.value}>
-              {color.label}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="form-group mb-2">
-        <label>Rok výroby</label>
-        <div className="row">
-          <div className="col-6">
-            <select
-              onChange={handleChange}
-              name="yearFrom"
-              className="form-control mb-2"
-            >
-              <option value={filter.yearFrom ? filter.yearFrom : null}>
-                {filter.yearFrom ? filter.yearFrom : 'Od'}
+            <option value="">-- Palivo --</option>
+            {FUEL.map((item) => (
+              <option key={item.value} value={item.value}>
+                {item.label}
               </option>
-              <option value="">-- Od --</option>
-              {getYearsList('1950', new Date().getFullYear()).map(
-                (year, index) => (
-                  <option key={index} value={year.value}>
-                    {year.label}
-                  </option>
-                )
-              )}
-            </select>
-          </div>
-          <div className="col-6">
-            <select
-              onChange={handleChange}
-              name="yearTo"
-              className="form-control mb-2"
-            >
-              <option value={filter.yearTo ? filter.yearTo : null}>
-                {filter.yearTo ? filter.yearTo : 'Do'}
+            ))}
+          </select>
+        </div>
+      )}
+      {['1', '2', '3', '4', '5', '6'].includes(filter.category) && (
+        <div className="form-group mb-2">
+          <select
+            onChange={handleChange}
+            className="form-control"
+            name="transmision"
+          >
+            <option value={filter.transmision ? filter.transmision : null}>
+              {filter.transmision
+                ? decodeTransmision(filter.transmision)
+                : 'Prevodovka'}
+            </option>
+            <option value="">-- Prevodovka --</option>
+            {TRANSMISION.map((trans) => (
+              <option key={trans.value} value={trans.value}>
+                {trans.label}
               </option>
-              <option value="">-- Do --</option>
-              {getYearsList('1950', new Date().getFullYear()).map(
-                (year, index) => (
-                  <option key={index} value={year.value}>
-                    {year.label}
+            ))}
+          </select>
+        </div>
+      )}
+      {['1', '2', '3', '4', '5', '6', '7', '8', '16'].includes(
+        filter.category
+      ) && (
+        <div className="form-group mb-2">
+          <select onChange={handleChange} className="form-control" name="color">
+            <option value={filter.color ? filter.color : null}>
+              {filter.color ? decodeColor(filter.color) : 'Farba'}
+            </option>
+            <option value="">-- Farba --</option>
+            {COLORS.map((color, index) => (
+              <option key={index} value={color.value}>
+                {color.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+      {['1', '2', '3', '4', '5', '6', '7', '8', '16'].includes(
+        filter.category
+      ) && (
+        <>
+          <div className="form-group mb-2">
+            <label>Rok výroby</label>
+            <div className="row">
+              <div className="col-6">
+                <select
+                  onChange={handleChange}
+                  name="yearFrom"
+                  className="form-control mb-2"
+                >
+                  <option value={filter.yearFrom ? filter.yearFrom : null}>
+                    {filter.yearFrom ? filter.yearFrom : 'Od'}
                   </option>
-                )
-              )}
-            </select>
+                  <option value="">-- Od --</option>
+                  {getYearsList('1950', new Date().getFullYear()).map(
+                    (year, index) => (
+                      <option key={index} value={year.value}>
+                        {year.label}
+                      </option>
+                    )
+                  )}
+                </select>
+              </div>
+              <div className="col-6">
+                <select
+                  onChange={handleChange}
+                  name="yearTo"
+                  className="form-control mb-2"
+                >
+                  <option value={filter.yearTo ? filter.yearTo : null}>
+                    {filter.yearTo ? filter.yearTo : 'Do'}
+                  </option>
+                  <option value="">-- Do --</option>
+                  {getYearsList('1950', new Date().getFullYear()).map(
+                    (year, index) => (
+                      <option key={index} value={year.value}>
+                        {year.label}
+                      </option>
+                    )
+                  )}
+                </select>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {filter.category && (
+        <div className="form-group mb-2">
+          <label>Cena</label>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="number"
+                onChange={handleChange}
+                name="priceFrom"
+                className="form-control mb-2"
+                placeholder="Od"
+                value={filter.priceFrom}
+              ></input>
+            </div>
+            <div className="col-6">
+              <input
+                type="number"
+                onChange={handleChange}
+                name="priceTo"
+                className="form-control mb-2"
+                placeholder="Do"
+                value={filter.priceTo}
+              ></input>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="form-group mb-2">
-        <label>Cena</label>
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="number"
-              onChange={handleChange}
-              name="priceFrom"
-              className="form-control mb-2"
-              placeholder="Od"
-              value={filter.priceFrom}
-            ></input>
-          </div>
-          <div className="col-6">
-            <input
-              type="number"
-              onChange={handleChange}
-              name="priceTo"
-              className="form-control mb-2"
-              placeholder="Do"
-              value={filter.priceTo}
-            ></input>
-          </div>
-        </div>
-      </div>
-      <div className="form-group mb-2">
-        <label>Najazdených km</label>
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="number"
-              onChange={handleChange}
-              name="kmFrom"
-              className="form-control mb-2"
-              placeholder="Od"
-              value={filter.kmFrom}
-            ></input>
-          </div>
-          <div className="col-6">
-            <input
-              type="number"
-              onChange={handleChange}
-              name="kmTo"
-              className="form-control mb-2"
-              placeholder="Do"
-              value={filter.kmTo}
-            ></input>
+      )}
+      {['1', '2', '3', '4', '5', '6', '7', '8', '16'].includes(
+        filter.category
+      ) && (
+        <div className="form-group mb-2">
+          <label>Najazdených km</label>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="number"
+                onChange={handleChange}
+                name="kmFrom"
+                className="form-control mb-2"
+                placeholder="Od"
+                value={filter.kmFrom}
+              ></input>
+            </div>
+            <div className="col-6">
+              <input
+                type="number"
+                onChange={handleChange}
+                name="kmTo"
+                className="form-control mb-2"
+                placeholder="Do"
+                value={filter.kmTo}
+              ></input>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="form-group mb-2">
-        <label>Výkon (kW)</label>
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="number"
-              onChange={handleChange}
-              name="powerFrom"
-              className="form-control mb-2"
-              placeholder="Od"
-              value={filter.powerFrom}
-            ></input>
-          </div>
-          <div className="col-6">
-            <input
-              type="number"
-              onChange={handleChange}
-              name="powerTo"
-              className="form-control mb-2"
-              placeholder="Do"
-              value={filter.powerTo}
-            ></input>
+      )}
+      {['1', '2', '3', '4', '5', '6', '7', '8', '16'].includes(
+        filter.category
+      ) && (
+        <div className="form-group mb-2">
+          <label>Výkon (kW)</label>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="number"
+                onChange={handleChange}
+                name="powerFrom"
+                className="form-control mb-2"
+                placeholder="Od"
+                value={filter.powerFrom}
+              ></input>
+            </div>
+            <div className="col-6">
+              <input
+                type="number"
+                onChange={handleChange}
+                name="powerTo"
+                className="form-control mb-2"
+                placeholder="Do"
+                value={filter.powerTo}
+              ></input>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="form-group text-center">
-        <button type="submit" className={button}>
-          Hľadať
-        </button>
-      </div>
+      )}
+      {filter.category && (
+        <div className="form-group text-center">
+          <button type="submit" className={button}>
+            Hľadať
+          </button>
+        </div>
+      )}
     </form>
   );
 };
